723e0255808ee0655fbe574036f139873c8e9c74
Kim Lesieur <lesieurkim@gmail.com>
2023-02-08 14:17:16 +0100

    feat: add readone, update and delete on Form entity

--- server/src/graphql/resolvers/form.resolver.ts
+++ server/src/graphql/resolvers/form.resolver.ts
@@ -12,6 +12,12 @@ export default class FormResolver {
         return forms;
     }
 
+    @Query(() => Form) 
+    async readOneForm(@Arg("id") id: string): Promise<Form> {
+        const Forms = await new FormService().readOne(+id);
+        return Forms;
+    }
+
     @Mutation(() => Form)
     async createForm(@Arg("createFormInput") createFormInput: CreateFormInput) : Promise<Form> {
 
@@ -20,5 +26,19 @@ export default class FormResolver {
         }        
         return await new FormService().create({...createFormInput});
     }
+
+    @Mutation(() => ResponseMessage )
+    async updateForm(@Arg("updateFormInput") {formId, ...updateFormInput}: UpdateFormInput): Promise<ResponseMessage>{
+         if((updateFormInput.title != null) && (updateFormInput.title.length > 100 || updateFormInput.title.length === 0)) {
+            throw new Error ( "the name should be between 1 and 100 characters")
+        }   
+        return await new FormService().update({formId, ...updateFormInput});
+    }
+
+    @Mutation(() => ResponseMessage) // retournera {success: true, message: ""}
+    async deleteForm(@Arg("id") id: string): Promise<ResponseMessage> {
+        const response = await new FormService().delete(+id);
+        return response;
+    }    
     
 }
