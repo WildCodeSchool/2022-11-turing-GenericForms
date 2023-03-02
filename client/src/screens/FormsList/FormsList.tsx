8800c93179aeb49307f23d29147327dee5d5f52c
Kim Lesieur <lesieurkim@gmail.com>
2023-03-02 16:03:44 +0100

    style: align items list horizontal/vertical + refactor CSS in page

--- client/src/screens/FormsList/FormsList.tsx
+++ client/src/screens/FormsList/FormsList.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './FormsList.css';
 import { Button, Grid, Typography  } from '@mui/material';
 import { makeStyles } from '@mui/styles';
 import { useQuery } from '@apollo/client';
@@ -18,11 +17,37 @@ interface FormsListProps {};
 
 const useStyles = makeStyles(styles);
 
+const css = {
+    formsListContainer: {
+        border: '1px solid #000000',
+        height: '8vh',
+        margin: '1vh 1vw',
+    },
+    centerTxt: {
+        textAlign: 'center',
+    },
+    row2: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        paddingLeft: '4vw',
+        height: '10vh',
+    },
+    row3: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '5vh',
+    },
+    formsRow:{
+        justifyContent: 'center',
+    }
+};
+
 function FormsList({}: FormsListProps) {
-
     const classes = useStyles();
-
-    //TODO get user form's data with Apollo Client
 
     const {data: formsData, loading, error, refetch} = useQuery<ReadFormsDTO>(READ_FORMS, {
         //authorization token in header set automatically ? 
@@ -36,7 +61,7 @@ function FormsList({}: FormsListProps) {
 
     return (
         <>
-            <Grid container className='dashboard-header-row2' >
+            <Grid container sx={css.row2} >
                 <Grid item xs={9} >
                     <Button variant='contained'>
                         + Créer un formulaire
@@ -46,49 +71,49 @@ function FormsList({}: FormsListProps) {
                     <Typography variant="h5">Order by</Typography>
                 </Grid>
             </Grid>
-            <Grid container className='dashboard-header-row3' >
+            <Grid container sx={css.row3} >
                 <Grid item xs={6} />
-                <Grid item xs={1} className=''>
-                    <Typography variant="label" >Questions</Typography>
+                <Grid item xs={1} >
+                    <Typography variant="label" component={'p'} sx={css.centerTxt} >Questions</Typography>
                 </Grid>
-                <Grid item xs={1} className=''>
-                    <Typography variant="label" >Réponses</Typography>
+                <Grid item xs={1} >
+                    <Typography variant="label" component={'p'} sx={css.centerTxt} >Réponses</Typography>
                 </Grid>
-                <Grid item xs={2} className=''>
-                    <Typography variant="label"  >Modifié le</Typography>
+                <Grid item xs={2} >
+                    <Typography variant="label" component={'p'} sx={css.centerTxt} >Modifié le</Typography>
                 </Grid>
-                <Grid item xs={1} className=''>
-                    <Typography variant="label" >Actif</Typography>
+                <Grid item xs={1} >
+                    <Typography variant="label" component={'p'} sx={css.centerTxt} >Actif</Typography>
                 </Grid>
-                <Grid item xs={1} className='' />
+                <Grid item xs={1}  />
             </Grid>
             
             {
                 formsData?.readForms.map((form) => (
-                    <Grid container key={form.formId} className='forms-list-container'>
-                        <Grid item xs={2} className=''>
+                    <Grid container key={form.formId} sx={css.formsListContainer} alignContent="center" alignItems="center" >
+                        <Grid item xs={2} display="flex" justifyContent="center">
                             <LocalPostOfficeIcon fontSize='large' color='secondary'/>
                         </Grid>
                         <Grid item xs={2} className={classes.title}>
-                            <Typography variant="body1">{form.title}</Typography>
+                            <Typography variant="body1" sx={css.centerTxt}>{form.title}</Typography>
                         </Grid>
-                        <Grid item xs={2} className=''>
-                            <Typography variant="body1">{form.category}</Typography>
+                        <Grid item xs={2}>
+                            <Typography variant="body1" sx={css.centerTxt}>{form.category}</Typography>
                         </Grid>
-                        <Grid item xs={1} className=''>
-                            <Typography variant="body1">55</Typography>
+                        <Grid item xs={1}>
+                            <Typography variant="body1" sx={css.centerTxt}>55</Typography>
                         </Grid>
-                        <Grid item xs={1} className=''>
-                            <Typography variant="body1">55</Typography>
+                        <Grid item xs={1}>
+                            <Typography variant="body1" sx={css.centerTxt}>55</Typography>
                         </Grid>
-                        <Grid item xs={2} className=''>
-                            <Typography variant="body1">{form.themeId}</Typography>
+                        <Grid item xs={2}>
+                            <Typography variant="body1" sx={css.centerTxt}>{form.themeId}</Typography>
                         </Grid>
-                        <Grid item xs={1} className=''>
-                            <Typography variant="body1">Oui</Typography>
+                        <Grid item xs={1}>
+                            <Typography variant="body1" sx={css.centerTxt}>Oui</Typography>
                         </Grid>
-                        <Grid item xs={1} className=''>
-                        <MoreVertIcon fontSize='large' color='disabled' />
+                        <Grid item xs={1} display="flex" justifyContent="center">
+                            <MoreVertIcon fontSize='large' color='disabled' />
                         </Grid>
                     </Grid>
                 ))
