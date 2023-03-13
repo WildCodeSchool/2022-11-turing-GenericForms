76a3880917b6cb7f9f178cf62add4001bdb088da
Kim Lesieur <lesieurkim@gmail.com>
2023-03-13 15:39:02 +0100

    feat: add handlers to add/remove choices and change text(no mutation for now)

--- client/src/components/QuestionPreview/SelectQuestionPreview.tsx
+++ client/src/components/QuestionPreview/SelectQuestionPreview.tsx
@@ -1,7 +1,10 @@
-import { Grid, List, ListItem, TextField } from '@mui/material';
+import { Grid, IconButton, List, ListItem, TextField } from '@mui/material';
 import React from 'react';
 import { FormDTO } from '../../types/form';
 import { QuestionDTO } from '../../types/question';
+import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
+import RemoveCircleRoundedIcon from '@mui/icons-material/RemoveCircleRounded';
+
 
 interface SelectQuestionPreviewProps {
     question: QuestionDTO; //? provient du FormContext
@@ -9,7 +12,9 @@ interface SelectQuestionPreviewProps {
 };
 
 const SelectQuestionPreview = ({question, setFormContext}: SelectQuestionPreviewProps) => {
+    const [choiceValue, setChoiceValue] = React.useState<string>("");
     
+    //TODO add a debouncer wrapper to avoid too many calls to the server
     const handleChangeTitle = (event: React.ChangeEvent<HTMLInputElement>) => {
         setFormContext((formContext: FormDTO) => {
             return {
@@ -28,6 +33,18 @@ const SelectQuestionPreview = ({question, setFormContext}: SelectQuestionPreview
         });
     };
 
+    const handleAddChoice = () => {
+        console.log("add choice with text: ", choiceValue);
+    };
+
+    const handleRemoveChoice = () => {
+        console.log("remove choice");
+    };
+
+    const handleChangeNewChoice = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setChoiceValue(event.target.value);
+    };
+
 
 
     return (
@@ -40,16 +57,36 @@ const SelectQuestionPreview = ({question, setFormContext}: SelectQuestionPreview
             </Grid>
             <Grid item xs={12}>
                 <List key={question.questionId}>
+                {question.choices.length === 0 && 
+                    <ListItem key="no-choice">
+                        <TextField id="standard-basic" variant="standard" placeholder='Choix 1'/>
+                    </ListItem>
+                }
                 {question.choices && question.choices.map((choice, index) => {
                     return (
                         <ListItem>
                             <TextField id="standard-basic" variant="standard" value={choice.text} key={choice.text+index}/>
+                            <IconButton onClick={handleRemoveChoice}>
+                                <RemoveCircleRoundedIcon />
+                            </IconButton>
                         </ListItem>
                     )
                 })
                 }
+                    {/*TODO complete the feature to add a choice */}
+                    <ListItem key={"add-choice"} >
+                        <IconButton onClick={handleAddChoice}>
+                            <AddCircleRoundedIcon />
+                        </IconButton>
+                        <TextField 
+                                id="standard-basic" 
+                                variant="standard" 
+                                value={choiceValue} 
+                                placeholder={"Ajouter un choix"}
+                                onChange={handleChangeNewChoice}
+                        />
+                    </ListItem>
                  </List>
-                {/* add a feature to add a choice */}
             </Grid>
         </Grid>
     )
