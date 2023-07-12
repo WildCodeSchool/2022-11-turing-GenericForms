c065c04eafc6c258f9297d92fc1cce062f7cca34
Kim <k.lesieur@beapp.fr>
2023-07-12 18:26:11 +0200

    fix(ESLint): ignore files

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -15,6 +15,11 @@ module.exports = {
    rules: {
        '@typescript-eslint/no-misused-promises': "off",
        '@typescript-eslint/no-unsafe-assignment': 'off',
+       '@typescript-eslint/no-unsafe-return': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        "@typescript-eslint/no-empty-function": "off",
    },
   root: true,
+  ignorePatterns: [".eslintrc.js", "jest.config.js", "", "**/build/**", "**/drafts/**"],
 };
