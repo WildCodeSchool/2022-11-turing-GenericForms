186714f37ef1b1b0142d41a10c7a58d9fd689031
Kim <k.lesieur@beapp.fr>
2023-08-23 09:37:51 +0200

    fix(App): redirect homepage to Login screen

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -18,6 +18,7 @@ function App() {
       <UserProvider>
         <EditFormProvider>
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
               <Route
